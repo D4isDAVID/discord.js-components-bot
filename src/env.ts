9d4d6cf4fc075a4510348d37630adeb1386ab440
David Malchin <malchin459@gmail.com>
2023-05-15 15:27:58 +0300

    Require dotenv from scripts and add `build:watch` script

--- src/env.ts
+++ src/env.ts
@@ -3,9 +3,6 @@ import { REST } from '@discordjs/rest';
 import { WebSocketManager } from '@discordjs/ws';
 import { env } from 'node:process';
 
-import dotenv from 'dotenv';
-dotenv.config();
-
 export const botToken = env.BOT_TOKEN ?? '';
 export const shardCount = env.SHARD_COUNT ? parseInt(env.SHARD_COUNT) : null;
 
